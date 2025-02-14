3d37c240924e75e0240619d9f0634113a52b53df
Nattan Aguia <nattan23@msn.com>
2025-02-13 20:39:51 -0500

    server

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import express from 'express';
 import routes from './routes/index.js';
-import connectDB from './config/connection.js';
-import { MongoClient } from 'mongodb';
+import db from './config/connection.js';
 
-connectDB();
+db();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Connection string to local instance of MongoDB
-const connectionStringURI = `mongodb://127.0.0.1:27017`;
-
-// Initialize a new instance of MongoClient
-const client = new MongoClient(connectionStringURI);
-
-const dbName = 
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
