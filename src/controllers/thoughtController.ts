1f504020c0bf7ba5f414700501b090ec3f4d73a9
Nattan Aguia <nattan23@msn.com>
2025-02-13 20:31:36 -0500

    thought controllers

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -1,44 +1,53 @@
-import { Thought } from '../models/index'
-import { User } from '../models/index'
-
+import Thought from '../models/Thought.js';
+import User from '../models/User.js';
 import { Request, Response } from 'express';
 
-export const getAllThoughts = async (req: Request, res: Response) => {
+export const getAllThoughts = async (_req: Request, res: Response) => {
     try {
-        const thoughts = await Thought.find({})
-        res.status(200).json(thoughts)
-    } catch (error) {
-        console.log('Uh Oh, something went wrong');
-        res.status(500).json({ error: 'Something went wrong' });
+        const thoughts = await Thought.find();
+        res.json(thoughts);
+    } catch (err) {
+        res.status(500).json({ message: `Failed to get thoughts. ${err}` });
     }
-}
+};
 
 export const getSingleThought = async (req: Request, res: Response) => {
-    const { id } = req.params;
+    try {
+        // get thought by object id from request parameters
+        const thought = await Thought.findById(req.params.id);
 
-   try {
-    const user = await Thought.findOne({ _id: id })
-    res.json(user);
-   } catch (error) {
-    res.status(500).json(error);
-   }
-}
+        if (!thought) {
+            res.status(404).json({ message: 'Thought not found.' });
+        }else{
+            res.json(thought);
+        }
+    } catch (err) {
+        res.status(500).json({ message: `Failed to get thought. ${err}` });
+    }
+};
 
 export const createThought = async (req: Request, res: Response) => {
     try {
-      const newThought = new Thought(req.body);
-      await newThought.save();
-      const user = await User.findOne({ username: newThought.username });
-      user?.thoughts.push(newThought._id as any);
-      await user?.save();
-      res.status(201).json(newThought);
+        const thought = await Thought.create(req.body);
+
+        // add thought to user's thoughts array
+        const user = await User.findByIdAndUpdate(
+            req.body.userId,
+            { $push: { thoughts: thought._id } },
+            { new: true }
+        );
+        
+        if(!user) {
+            res.status(404).json({ message: 'User not found.' });
+        } else{
+            res.json(thought);
+        }
     } catch (err) {
-      res.status(400).json({ error: (err as Error).message });
+        res.status(500).json({ message: `Failed to create thought. ${err}` });
     }
-  };
+};
 
 export const updateThought = async (req: Request, res: Response) => {
-
     try {
         const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.json(thought);
